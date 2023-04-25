2347edaab4816ad484498824cf3c3172e52f07e5
MatHennel <68912012+Ryssia@users.noreply.github.com>
2023-04-25 17:59:31 -0300

    validação de nome e id

--- lib/form_contact_fielder.dart
+++ lib/form_contact_fielder.dart
@@ -1,18 +1,18 @@
-import 'package:contact_crud_hive/model/user.dart';
 import 'package:flutter/material.dart';
+import 'package:contact_crud_hive/model/user.dart';
 
 class FormContactFielder extends StatelessWidget {
-  TextEditingController controller;
-  String hintTextName;
-  IconData iconData;
-  TextInputType textInputType;
+  final TextEditingController controller;
+  final String hintTextName;
+  final IconData iconData;
+  final TextInputType textInputType;
 
-  FormContactFielder(
-      {super.key,
-      required this.controller,
-      required this.hintTextName,
-      required this.iconData,
-      this.textInputType = TextInputType.text});
+  FormContactFielder({
+    required this.controller,
+    required this.hintTextName,
+    required this.iconData,
+    this.textInputType = TextInputType.text,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -31,13 +31,31 @@ class FormContactFielder extends StatelessWidget {
         if (hintTextName == 'Email' && !validateEmail(value)) {
           return 'Digite um Email Válido';
         }
+        if (hintTextName == 'Nome' && !validaApenasLetras(value)) {
+          return 'Por favor, digite apenas letras';
+        }
+        if (hintTextName == 'Código' && !validaId(value)) {
+          return 'Por favor, digite apenas números';
+        }
+        return null;
       },
     );
   }
+
+  bool validateEmail(String email) {
+    final emailReg = RegExp(
+        r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)*$");
+    return emailReg.hasMatch(email);
+  }
+
+  bool validaApenasLetras(String value) {
+    final RegExp regex = RegExp(r'^[a-zA-Z]+$');
+    return regex.hasMatch(value);
+  }
+
+  bool validaId(String id) {
+    final RegExp regex = RegExp(r'^[0-9]+$');
+    return regex.hasMatch(id);
+  }
 }
 
-validateEmail(String email) {
-  final emailReg = RegExp(
-      r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)*$");
-  return emailReg.hasMatch(email);
-}
